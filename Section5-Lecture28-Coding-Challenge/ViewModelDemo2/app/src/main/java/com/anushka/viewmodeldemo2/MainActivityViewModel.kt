0088083d121465eb12b7c40116a317c6a78632f3
LeaCuvelo <lea.cuvelo@gmail.com>
2022-07-26 14:56:29 -0300

    Added Section5 Lecture 28 - CodingChallenge

---
+++ Section5-Lecture28-Coding-Challenge/ViewModelDemo2/app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModel.kt
@@ -0,0 +1,23 @@
+package com.anushka.viewmodeldemo2
+
+import androidx.databinding.Bindable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel(startingTotal : Int) : ViewModel() {
+     private var total = MutableLiveData<Int>()
+     val totalData : LiveData<Int>
+     get() = total
+
+    val inputText: MutableLiveData<String> = MutableLiveData<String>()
+
+    init {
+        total.value = startingTotal
+    }
+
+    fun setTotal(){
+        val intInput : Int = inputText.value!!.toInt()
+        total.value =(total.value)?.plus(intInput)
+    }
+}
